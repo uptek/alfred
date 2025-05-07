c75b3500df325f09088647fb541f45d8ff1de5d8
Junaid Ahmed <junaidkbr@gmail.com>
2025-05-07 21:59:18 +0500

    Add version

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   manifest: {
     name: 'Shopkeeper',
     description: 'Make Shopify great again!',
+    version: '1.0.0',
   },
   vite: () => ({
     plugins: [preact()],
