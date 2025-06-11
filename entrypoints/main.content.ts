e73ef0d1de21541297f5b780fb67fdb94a7892ee
Junaid Ahmed <junaidkbr@gmail.com>
2025-06-11 23:46:09 +0500

    Run main.content.ts at document_start

--- entrypoints/main.content.ts
+++ entrypoints/main.content.ts
@@ -1,5 +1,6 @@
 export default defineContentScript({
   matches: ['<all_urls>'],
+  runAt: 'document_start',
   async main() {
     await injectScript('/shopkeeper-main-world.js', {
       keepInDom: true,
