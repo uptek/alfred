40bab9bf757cae2baa8347cb24e7f7afa7a9b54a
Junaid Ahmed <junaidkbr@gmail.com>
2025-09-08 20:04:52 +0500

    Bump version

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   manifest: {
     name: 'Alfred for Shopify',
     description: 'Make Shopify great again!',
-    version: '2025.09.06',
+    version: '2025.09.08',
     action: {
       default_title: 'Alfred',
     },
